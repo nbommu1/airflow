a53f2b387ed443cff14a7cb15455597bfdf855c3
Niranjan Bommu <niranjanbommu@NiranjansPro.local>
2024-03-06 16:19:52 -0500

    adding test dag

---
+++ dags/test_nbommu.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+        'owner' : 'airflow',
+        'start_date' : datetime(2024, 02, 06),
+
+}
+
+dag = DAG(dag_id='DAG-1',
+        default_args=default_args,
+        schedule_interval='@once', 
+        catchup=False
+    )
+
+ start = DummyOperator(task_id = 'start', dag = dag)
+ end = DummyOperator(task_id = 'end', dag = dag)
+
+start >> end 
